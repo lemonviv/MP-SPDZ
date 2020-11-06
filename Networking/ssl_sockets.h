e8fafcb65e2c7418221d876756d103cab9643663
yunchengwu <lemonwyc@gmail.com>
2020-11-06 10:25:45 +0800

    Add new ssl_ctx initialization for library usage

--- Networking/ssl_sockets.h
+++ Networking/ssl_sockets.h
@@ -33,6 +33,19 @@ public:
         use_private_key_file(key_file, pem);
         add_verify_path(PREP_DIR);
     }
+    ssl_ctx(string prep_dir, string me) :
+        boost::asio::ssl::context(boost::asio::ssl::context::tlsv12)
+    {
+      string prefix = prep_dir + me;
+      string cert_file = prefix + ".pem";
+      string key_file = prefix + ".key";
+      check_ssl_file(cert_file);
+      check_ssl_file(key_file);
+
+      use_certificate_file(cert_file, pem);
+      use_private_key_file(key_file, pem);
+      add_verify_path(prep_dir);
+    }
 };
 
 class ssl_socket : public boost::asio::ssl::stream<boost::asio::ip::tcp::socket>
