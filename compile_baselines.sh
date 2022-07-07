93c716b4278a6749a5f402a99dd9f5a1594ecdd1
lemonviv <lemonwyc@gmail.com>
2022-07-07 18:11:19 +0800

    Add compile script

---
+++ compile_baselines.sh
@@ -0,0 +1,89 @@
+
+#/bin/bash
+
+./compile.py Programs/Source/spdz_logistic_reg_5party.mpc
+echo "done compile spdz_logistic_reg_5party"
+
+./compile.py Programs/Source/spdz_logistic_reg_6party.mpc
+echo "done compile spdz_logistic_reg_6party"
+
+./compile.py Programs/Source/spdz_logistic_reg_partyfeature10.mpc
+echo "done compile spdz_logistic_reg_partyfeature10"
+
+./compile.py Programs/Source/spdz_logistic_reg_partyfeature20.mpc
+echo "done compile spdz_logistic_reg_partyfeature20"
+
+./compile.py Programs/Source/spdz_logistic_reg_partyfeature30.mpc
+echo "done compile spdz_logistic_reg_partyfeature30"
+
+./compile.py Programs/Source/spdz_logistic_reg_partyfeature40.mpc
+echo "done compile spdz_logistic_reg_partyfeature40"
+
+./compile.py Programs/Source/spdz_logistic_reg_partyfeature50.mpc
+echo "done compile spdz_logistic_reg_partyfeature50"
+
+./compile.py Programs/Source/spdz_mlp_2party.mpc
+echo "done compile spdz_mlp_2party"
+
+./compile.py Programs/Source/spdz_mlp_3party.mpc
+echo "done compile spdz_mlp_3party"
+
+./compile.py Programs/Source/spdz_mlp_4party.mpc
+echo "done compile spdz_mlp_4party"
+
+./compile.py Programs/Source/spdz_mlp_5party.mpc
+echo "done compile spdz_mlp_5party"
+
+./compile.py Programs/Source/spdz_mlp_6party.mpc
+echo "done compile spdz_mlp_6party"
+
+./compile.py Programs/Source/spdz_mlp_partyfeature10.mpc
+echo "done compile spdz_mlp_partyfeature10"
+
+./compile.py Programs/Source/spdz_mlp_partyfeature20.mpc
+echo "done compile spdz_mlp_partyfeature20"
+
+./compile.py Programs/Source/spdz_mlp_partyfeature30.mpc
+echo "done compile spdz_mlp_partyfeature30"
+
+./compile.py Programs/Source/spdz_mlp_partyfeature40.mpc
+echo "done compile spdz_mlp_partyfeature40"
+
+./compile.py Programs/Source/spdz_mlp_partyfeature50.mpc
+echo "done compile spdz_mlp_partyfeature50"
+
+./compile.py Programs/Source/LIME-MPC-LR_nsample2000_l1.mpc
+echo "done compile LIME-MPC-LR_nsample2000_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_nsample2000_l2.mpc
+echo "done compile LIME-MPC-LR_nsample2000_l2"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature10_l1.mpc
+echo "done compile LIME-MPC-LR_partyfeature10_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature10_l2.mpc
+echo "done compile LIME-MPC-LR_partyfeature10_l2"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature20_l1.mpc
+echo "done compile LIME-MPC-LR_partyfeature20_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature20_l2.mpc
+echo "done compile LIME-MPC-LR_partyfeature20_l2"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature30_l1.mpc
+echo "done compile LIME-MPC-LR_partyfeature30_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature30_l2.mpc
+echo "done compile LIME-MPC-LR_partyfeature30_l2"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature40_l1.mpc
+echo "done compile LIME-MPC-LR_partyfeature40_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature40_l2.mpc
+echo "done compile LIME-MPC-LR_partyfeature40_l2"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature50_l1.mpc
+echo "done compile LIME-MPC-LR_partyfeature50_l1"
+
+./compile.py Programs/Source/LIME-MPC-LR_partyfeature50_l2.mpc
+echo "done compile LIME-MPC-LR_partyfeature50_l2"
