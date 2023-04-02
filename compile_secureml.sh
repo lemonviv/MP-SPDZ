da4bdd76271fc64fdb989e52a6fe35d1b55f84a0
lemonviv <lemonwyc@gmail.com>
2023-04-02 10:20:21 +0800

    Update iterations

---
+++ compile_secureml.sh
@@ -0,0 +1,62 @@
+
+#/bin/bash
+
+./compile.py Programs/Source/secureml_logistic_reg_2party.mpc
+echo "done compile secureml_logistic_reg_2party"
+
+./compile.py Programs/Source/secureml_logistic_reg_3party.mpc
+echo "done compile secureml_logistic_reg_3party"
+
+./compile.py Programs/Source/secureml_logistic_reg_4party.mpc
+echo "done compile secureml_logistic_reg_4party"
+
+./compile.py Programs/Source/secureml_logistic_reg_5party.mpc
+echo "done compile secureml_logistic_reg_5party"
+
+./compile.py Programs/Source/secureml_logistic_reg_6party.mpc
+echo "done compile secureml_logistic_reg_6party"
+
+./compile.py Programs/Source/secureml_logistic_reg_partyfeature10.mpc
+echo "done compile secureml_logistic_reg_partyfeature10"
+
+./compile.py Programs/Source/secureml_logistic_reg_partyfeature20.mpc
+echo "done compile secureml_logistic_reg_partyfeature20"
+
+./compile.py Programs/Source/secureml_logistic_reg_partyfeature30.mpc
+echo "done compile secureml_logistic_reg_partyfeature30"
+
+./compile.py Programs/Source/secureml_logistic_reg_partyfeature40.mpc
+echo "done compile secureml_logistic_reg_partyfeature40"
+
+./compile.py Programs/Source/secureml_logistic_reg_partyfeature50.mpc
+echo "done compile secureml_logistic_reg_partyfeature50"
+
+./compile.py Programs/Source/secureml_mlp_2party.mpc
+echo "done compile secureml_mlp_2party"
+
+./compile.py Programs/Source/secureml_mlp_3party.mpc
+echo "done compile secureml_mlp_3party"
+
+./compile.py Programs/Source/secureml_mlp_4party.mpc
+echo "done compile secureml_mlp_4party"
+
+./compile.py Programs/Source/secureml_mlp_5party.mpc
+echo "done compile secureml_mlp_5party"
+
+./compile.py Programs/Source/secureml_mlp_6party.mpc
+echo "done compile secureml_mlp_6party"
+
+./compile.py Programs/Source/secureml_mlp_partyfeature10.mpc
+echo "done compile secureml_mlp_partyfeature10"
+
+./compile.py Programs/Source/secureml_mlp_partyfeature20.mpc
+echo "done compile secureml_mlp_partyfeature20"
+
+./compile.py Programs/Source/secureml_mlp_partyfeature30.mpc
+echo "done compile secureml_mlp_partyfeature30"
+
+./compile.py Programs/Source/secureml_mlp_partyfeature40.mpc
+echo "done compile secureml_mlp_partyfeature40"
+
+./compile.py Programs/Source/secureml_mlp_partyfeature50.mpc
+echo "done compile secureml_mlp_partyfeature50"
